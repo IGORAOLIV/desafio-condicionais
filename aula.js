267e17e769e56dc4d24e23b5072d0444d2764736
IGORAOLIV <igoralmeida0105@gmail.com>
2023-05-03 17:30:14 -0300

    primeiro commit

---
+++ aula.js
@@ -0,0 +1,11 @@
+const numero = 3;
+
+const numeroPar = (numero % 2) === 0;
+
+if (numero === 0) {
+    console.log('O número é inválido.')
+} else if (numeroPar) {
+    console.log('Par');
+} else {
+    console.log('Impar')
+}
